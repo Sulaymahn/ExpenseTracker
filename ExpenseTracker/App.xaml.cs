0be7bf905362d60904f5e1a5ae983acbd6fcca48
Sulaymahn Mukhtar <61796685+Sulaymahn@users.noreply.github.com>
2021-02-27 04:19:37 +0100

    Add project files.

---
+++ ExpenseTracker/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTracker.Pages;
+using Xamarin.Forms;
+
+namespace ExpenseTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TestView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ExpenseTracker/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Poppins-Black.ttf", Alias = "Poppins-Black")]
+[assembly: ExportFont("Poppins-Bold.ttf", Alias = "Poppins-Bold")]
+[assembly: ExportFont("Poppins-ExtraBold.ttf", Alias = "Poppins-ExtraBold")]
+[assembly: ExportFont("Poppins-Medium.ttf", Alias = "Poppins-Medium")]
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "Poppins-Regular")]
+[assembly: ExportFont("Poppins-SemiBold.ttf", Alias = "Poppins-SemiBold")]
